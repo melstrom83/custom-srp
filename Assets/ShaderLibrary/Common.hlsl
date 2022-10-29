2c12de20f8769751bbaa527548320e807e05c843
Pavel Kirsanov <melstrom83@gmail.com>
2022-10-30 00:01:19 +0300

    Updated to Unity 2021.3.9

--- Assets/ShaderLibrary/Common.hlsl
+++ Assets/ShaderLibrary/Common.hlsl
@@ -5,6 +5,8 @@
 
 #define UNITY_MATRIX_M unity_ObjectToWorld
 #define UNITY_MATRIX_I_M unity_WorldToObject
+#define UNITY_PREV_MATRIX_M (float4x4)0
+#define UNITY_PREV_MATRIX_I_M (float4x4)0
 #define UNITY_MATRIX_V unity_MatrixV
 #define UNITY_MATRIX_VP unity_MatrixVP
 #define UNITY_MATRIX_P glstate_matrix_projection
